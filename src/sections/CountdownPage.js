f3fddf1d13121619bedea51bf51a6745ea37776d
poimenidou <irenepoim@hotmail.com>
2023-12-25 19:01:29 +0200

    removed random ad

--- src/sections/CountdownPage.js
+++ src/sections/CountdownPage.js
@@ -79,7 +79,6 @@ const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
     <div className="show-counter">
       <a
-        href="https://tapasadhikary.com"
         target="_blank"
         rel="noopener noreferrer"
         className="countdown-link"
